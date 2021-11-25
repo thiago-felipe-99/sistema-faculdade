7149b711c30023fc886fc80f0d3d07d2c149de37
Thiago Felipe <thiago.felipe@tutanota.com>
2021-11-25 15:48:42 -0400

    Adicionando o template do backend

---
+++ backend/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/pong", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ping",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		panic(err)
+	}
+}

4801fea13a8de80b1aabef90bc13103140591c58
Thiago Felipe <thiago.felipe@tutanota.com>
2022-01-13 07:13:48 -0400

    Arrumando a lógica de como criar um cpf aleatório

--- backend/aleatorio/main.go
+++ backend/aleatorio/main.go
@@ -3,6 +3,7 @@ package aleatorio
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"thiagofelipe.com.br/sistema-faculdade-backend/entidades"
 )
@@ -11,17 +12,42 @@ import (
 func Palavra(tamanho int) string {
 	letters := []rune("abcdefghijklmnopqrstuvwxyzáéíóúâêîôûãẽĩõũçABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚÂÊÎÔÛÃẼĨÕŨÇ") //nolint:lll
 
+	rand.Seed(time.Now().UnixNano())
+
 	s := make([]rune, tamanho)
 	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))] //nolint:gosec
+		s[i] = letters[rand.Intn(len(letters))] //nolint: GoSec
 	}
 
 	return string(s)
 }
 
+//nolint: gomnd
 // CPF cria um CPF aleatório.
 func CPF() entidades.CPF {
-	maxCPF := 99999999999
+	rand.Seed(time.Now().UnixNano())
 
-	return fmt.Sprintf("%011d", rand.Intn(maxCPF)) //nolint: GoSec
+	maxCPF := 999999999
+	CPFSemDigitos := fmt.Sprintf("%09d", rand.Intn(maxCPF)) //nolint: GoSec
+
+	digito1 := 0
+	digito2 := 0
+
+	for índice, número := range CPFSemDigitos {
+		digito1 += int(número-'0') * (9 - índice%10)
+		digito2 += int(número-'0') * (9 - (índice+1)%10)
+	}
+
+	digito1 = (digito1 % 11) % 10
+	digito2 += digito1 * 9
+	digito2 = (digito2 % 11) % 10
+
+	cpf := ""
+	for índice := 8; índice >= 0; índice-- {
+		cpf += string(CPFSemDigitos[índice])
+	}
+
+	cpf = fmt.Sprintf("%s%d%d", cpf, digito1, digito2)
+
+	return cpf
 }

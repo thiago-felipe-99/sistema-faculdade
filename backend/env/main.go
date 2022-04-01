ca9fa9badf7ffbb0989f2950c1dca239a0b10d65
Thiago Felipe <thiago.felipe@tutanota.com>
2022-04-01 07:01:10 -0400

    Refatorando o pacote env

--- backend/env/main.go
+++ backend/env/main.go
@@ -1,3 +1,4 @@
+// Package env representa como a aplicação lida com o ambiente que roda.
 package env
 
 import (
@@ -9,15 +10,19 @@ const (
 	portaPadrãoBDMatéria        = "9001"
 )
 
+// Portas representa as variáveis de ambiente do tipo porta.
 type Portas struct {
 	BDAdministrativo string
 	BDMateria        string
 }
 
+// VariáveisDeAmbiente são as variáveis do ambiente onde a aplicação roda.
 type VariáveisDeAmbiente struct {
 	Portas Portas
 }
 
+// PegandoVariáveisDeAmbiente retorna as variáveis de ambiente que a aplicação
+// precisa.
 func PegandoVariáveisDeAmbiente() (variáveis VariáveisDeAmbiente) {
 	variáveis.Portas.BDAdministrativo = os.Getenv("BD_ADMINISTRATIVO_PORT")
 	if variáveis.Portas.BDAdministrativo == "" {

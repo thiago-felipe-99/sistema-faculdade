cce19428b0e3fbacd5a7f91b838723b7e580a6ac
Thiago Felipe <thiago.felipe@tutanota.com>
2022-04-06 09:13:58 -0300

    Deicando globalmentes as envs do docker-compose

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 $SCRIPT_DIR/scripts/init-db-administrativo/init-db.sh
 
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' env-docker-compose | xargs)
 
 docker exec -i db-administrativo mysql --user root --password=$DB_ADMINISTRATIVO_ROOT_PASSWORD < $SCRIPT_DIR/scripts/init-db-administrativo/init.sql
 
-unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs -d '\n')
+unset $(grep -v '^#' env-docker-compose | sed -E 's/(.*)=.*/\1/' | xargs -d '\n')
 
